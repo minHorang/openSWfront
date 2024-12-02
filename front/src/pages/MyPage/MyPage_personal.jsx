e5d07b9fc318ed57c415981ff009cc1530747219
M2 <m2@gimminbeoms-MacBook-Pro.local>
2024-12-02 13:44:50 +0900

    fd

--- front/src/pages/MyPage/MyPage_personal.jsx
+++ front/src/pages/MyPage/MyPage_personal.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import './MyPage_personal.css';
+import './Mypage_personal.css';
 
 function MyPage_personal() {
   const [userData, setUserData] = useState({ id: '', email: '', image: null });
